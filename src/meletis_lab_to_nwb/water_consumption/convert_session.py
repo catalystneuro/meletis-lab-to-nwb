739cbaa1dcdc593e053875e7a8e81b2788a5a91c
weiglszonja <wszonja@gmail.com>
2026-05-07 12:57:55 +0200

    update video name to be the same as arrow maze choice task

--- src/meletis_lab_to_nwb/water_consumption/convert_session.py
+++ src/meletis_lab_to_nwb/water_consumption/convert_session.py
@@ -135,7 +135,7 @@ def session_to_nwb(
             video_file_path=video_file_path,
             metadata_yaml_path=behavior_metadata_yaml_path,
         ),
-        Video=dict(file_paths=[str(video_file_path)], video_name="BehaviorVideo"),
+        Video=dict(file_paths=[str(video_file_path)]),
     )
     conversion_options = dict(
         FiberPhotometry=dict(stub_test=stub_test),
@@ -188,8 +188,9 @@ def session_to_nwb(
 
     # Update camera device from behavior_metadata.yaml
     video_device = load_dict_from_file(behavior_metadata_yaml_path)["VideoDevice"]
-    metadata["Behavior"]["ExternalVideos"]["BehaviorVideo"]["device"]["name"] = video_device["name"]
-    metadata["Behavior"]["ExternalVideos"]["BehaviorVideo"]["device"]["description"] = video_device["description"]
+    video_key = f"Video {Path(video_file_path).stem}"
+    metadata["Behavior"]["ExternalVideos"][video_key]["device"]["name"] = video_device["name"]
+    metadata["Behavior"]["ExternalVideos"][video_key]["device"]["description"] = video_device["description"]
 
     converter.run_conversion(
         metadata=metadata,
