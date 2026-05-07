51f825a93a83568768d008e9745f762db7d01e65
weiglszonja <wszonja@gmail.com>
2026-05-07 12:45:11 +0200

    fix metadata key

--- src/meletis_lab_to_nwb/water_consumption/convert_session.py
+++ src/meletis_lab_to_nwb/water_consumption/convert_session.py
@@ -188,8 +188,8 @@ def session_to_nwb(
 
     # Update camera device from behavior_metadata.yaml
     video_device = load_dict_from_file(behavior_metadata_yaml_path)["VideoDevice"]
-    metadata["Behavior"]["ExternalVideos"]["Video BehaviorVideo"]["device"]["name"] = video_device["name"]
-    metadata["Behavior"]["ExternalVideos"]["Video BehaviorVideo"]["device"]["description"] = video_device["description"]
+    metadata["Behavior"]["ExternalVideos"]["BehaviorVideo"]["device"]["name"] = video_device["name"]
+    metadata["Behavior"]["ExternalVideos"]["BehaviorVideo"]["device"]["description"] = video_device["description"]
 
     converter.run_conversion(
         metadata=metadata,
